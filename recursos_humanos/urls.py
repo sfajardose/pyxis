1ce2f90f9387e0f0e007c202dd4c30899697fc6e
f-arruza <f.arruza@uniandes.edu.co>
2018-01-20 15:50:07 -0500

    add loaderIO token

--- recursos_humanos/urls.py
+++ recursos_humanos/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import LoaderIO
 
 
 admin.site.site_header = 'Recursos Humanos'
@@ -7,4 +8,5 @@ admin.site.site_header = 'Recursos Humanos'
 urlpatterns = [
     url(r'^', admin.site.urls),
     url(r'^recursos/', include('recursos.urls')),
+    url(r'^loaderio-65e703fb3f6975d8db96b9c226cf3648/', LoaderIO.as_view(), name='loarderio'),
 ]
---
+++ recursos_humanos/views.py
@@ -0,0 +1,5 @@
+from django.views.generic import TemplateView
+
+
+class LoaderIO(TemplateView):
+    template_name = 'loaderio-65e703fb3f6975d8db96b9c226cf3648.html'
